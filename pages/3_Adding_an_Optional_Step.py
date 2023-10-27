1bfe0cc19204e4d33b63d0441c341b9569e0ce12
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-10-27 18:59:06 +0100

    Add diagram for exercise 3

--- pages/3_Adding_an_Optional_Step.py
+++ pages/3_Adding_an_Optional_Step.py
@@ -5,7 +5,7 @@ Allows users to interact with an increasingly more complex treatment simulation
 '''
 import streamlit as st
 
-from helper_functions import read_file_contents, add_logo
+from helper_functions import read_file_contents, add_logo, mermaid
 from model_classes import *
 
 
@@ -23,5 +23,21 @@ with open("style.css") as css:
 ## We add in a title for our web app's page
 st.title("Discrete Event Simulation Playground")
 st.subheader("Making Patients Behave Differently: Adding in an Optional Step")
+
+mermaid(height=125, code=
+    """
+            %%{ init: { 'flowchart': { 'curve': 'step' } } }%%
+            %%{ init: {  'theme': 'base', 'themeVariables': {'lineColor': '#b4b4b4'} } }%%
+            flowchart LR
+            A[Arrival]--> B[Advice -\nNurse]
+            B--> C[Treatment -\nNurse]
+            B --> Z[Discharge]
+            C --> Z
+
+            classDef default font-size:18pt,font-family:lexend;
+            linkStyle default stroke:white;
+        """
+    )
+
 
 st.markdown(read_file_contents('resources/entity_paths_text.md'))
