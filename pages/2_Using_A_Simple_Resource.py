301d7ff43658542524a756b561ad77f95f997628
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-10-27 18:58:57 +0100

    Add sliders for exercise 2

--- pages/2_Using_A_Simple_Resource.py
+++ pages/2_Using_A_Simple_Resource.py
@@ -44,9 +44,44 @@ with tab1:
     st.markdown(read_file_contents('resources/first_simple_resource.md'))
 
 with tab2:
-     st.markdown(read_file_contents('resources/first_simple_resource_exercise.md'))
+    st.markdown(read_file_contents('resources/first_simple_resource_exercise.md'))
 
 
 with tab3:
-    st.markdown("placeholder")
+    col1, col2 = st.columns([0.5, 1.5])
+
+    with col1:
+        nurses = st.slider("How Many Nurses Are Available?", 1, 50, step=1, value=5)
+
+        consult_time = st.slider("How long (in minutes) does a consultation take on average?",
+                                    5, 60, step=5, value=20)
+
+        consult_time_sd = st.slider("How much (in minutes) does the time for a consultation usually vary by?",
+                                    5, 30, step=5, value=10)
+
+        paperwork = st.slider("Should a fixed time for paperwork be allowed?", 0, 50, step=5, value=0)
+
+        night_nurse = st.toggle(label="Should the number of nurses halve overnight?")
+
+        st.subheader("Previous Parameters")
+
+
+        seed = st.number_input("Set a random number for the computer to start from",
+                        1, 100000,
+                        step=1, value=42)
+        
+        n_reps = st.slider("How many times should the simulation run?",
+                           1, 50,
+                           step=1, value=10)
+        run_time_days = st.slider("How many days should we run the simulation for each time?",
+                                  1, 100,
+                                  step=1, value=15)
+
+     
+        mean_arrivals_per_day = st.slider("How many patients should arrive per day on average?",
+                                          10, 1000,
+                                          step=5, value=300)
+
+    with col2:
+        st.text("Placeholder")
 
