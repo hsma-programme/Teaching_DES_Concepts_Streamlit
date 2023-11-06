0f71cc8a1ead3d0335af81335a759ee8dd615668
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-11-06 15:08:16 +0000

    Change layout and split out diagrams

--- pages/3_🩹_Adding_an_Optional_Step.py
+++ pages/3_🩹_Adding_an_Optional_Step.py
@@ -26,29 +26,107 @@ with open("style.css") as css:
 st.title("Discrete Event Simulation Playground")
 st.subheader("Making Patients Behave Differently: Adding in an Optional Step")
 
-mermaid(height=225, code=
-    """
-            %%{ init: { 'flowchart': { 'curve': 'step' } } }%%
-            %%{ init: {  'theme': 'base', 'themeVariables': {'lineColor': '#b4b4b4'} } }%%
-            flowchart LR
-            
-            A[Arrival]----> B[Advice]
-            
-            B -.-> F([Nurse/Cubicle])
-            F -.-> B
-            
-            B----> C[Treatment]
+tab1, tab2, tab3 = st.tabs(["Introduction", "Exercise", "Playground"])
 
-            C -.-> G([Nurse/Cubicle])
-            G -.-> C
+with tab1:
 
-            B ----> Z[Discharge]
-            C ----> Z
+    st.markdown("""
+                Now, it's not as simple as all of our patients being looked at by a nurse and then sent on their merry way.
 
-            classDef default font-size:18pt,font-family:lexend;
-            linkStyle default stroke:white;
+                Some of them - but not all of them - may require another step where they undergo some treatment.
+                
+                So for some people, their pathway looks like this:
+                """)
+
+
+    mermaid(height=225, code=
+        """
+                %%{ init: { 'flowchart': { 'curve': 'step' } } }%%
+                %%{ init: {  'theme': 'base', 'themeVariables': {'lineColor': '#b4b4b4'} } }%%
+                flowchart LR
+                
+                A[Arrival]----> B[Advice]
+                
+                B -.-> F([Nurse/Cubicle])
+                F -.-> B
+                
+                B----> C[Treatment]
+
+                C -.-> G([Nurse/Cubicle])
+                G -.-> C
+
+                C ----> Z[Discharge]
+
+                classDef default font-size:18pt,font-family:lexend;
+                linkStyle default stroke:white;
+            """
+        )
+
+    st.markdown("But for other simpler cases, their pathway still looks like this!")
+
+    mermaid(height=225, code=
+        """
+                %%{ init: { 'flowchart': { 'curve': 'step' } } }%%
+                %%{ init: {  'theme': 'base', 'themeVariables': {'lineColor': '#b4b4b4'} } }%%
+                flowchart LR
+                
+                A[Arrival]----> B[Advice]
+                
+                B -.-> F([Nurse/Cubicle])
+                F -.-> B
+                
+                B ----> Z[Discharge]
+
+                classDef default font-size:18pt,font-family:lexend;
+                linkStyle default stroke:white;
+            """
+        )
+
+    st.markdown(
+        """
+        So how do we ensure that some of our patients go down one pathway and not the other?
+
+        You guessed it - the answer is sampling from a distribution again! 
+
+        We can tell the computer the rough split we'd like to say - let's say 30% of our patients need the treatment step, but the other 70% will 
+
+        And as before, there will be a bit of randomness, just like in the real world.
+        In one simulation, we might end up with a 69/31 split, and the next might be 72/28, but it will always be around the expected split we've asked for.
         """
     )
 
+    st.markdown(
+        """
+        We can think of our pathway as looking like this overall:
+        """
+    )
 
-st.markdown(read_file_contents('resources/entity_paths_text.md'))
+    mermaid(height=225, code=
+        """
+                %%{ init: { 'flowchart': { 'curve': 'step' } } }%%
+                %%{ init: {  'theme': 'base', 'themeVariables': {'lineColor': '#b4b4b4'} } }%%
+                flowchart LR
+                
+                A[Arrival]--> B[Advice]
+                
+                B -.-> F([Nurse/Cubicle])
+                F -.-> B
+                
+                B----> |30% of patients| C[Treatment]
+
+                C -.-> G([Nurse/Cubicle])
+                G -.-> C
+
+                B ----> |70% of patients| Z[Discharge]
+                C --> Z
+
+                classDef default font-size:18pt,font-family:lexend;
+                linkStyle default stroke:white;
+            """
+        )
+
+with tab2:
+    st.markdown(read_file_contents('resources/entity_paths_text.md'))
+
+with tab3:
+    st.markdown("Placeholder")
