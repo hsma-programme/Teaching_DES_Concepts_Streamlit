a492b1ed6c5e984afb09ed5d6420971ee136be44
Bergam0t <s.j.rosser@exeter.ac.uk>
2023-11-07 14:12:37 +0000

    Begin setting up optional step exercise

--- pages/3_🩹_Adding_an_Optional_Step.py
+++ pages/3_🩹_Adding_an_Optional_Step.py
@@ -129,4 +129,74 @@ with tab2:
     st.markdown(read_file_contents('resources/entity_paths_text.md'))
 
 with tab3:
-    st.markdown("Placeholder")
+
+    col1, col2 = st.columns([1,3])
+
+    with col1:
+        treat_p = st.slider("Probability that a patient will need treatment", 0.0, 1.0, step=0.01, value=0.7)
+
+        nurses_floating_resource = st.checkbox("Tick this box to make any nurse able to be used at any point in the process.")
+
+        if not nurses_floating_resource:
+            nurses_advice = st.slider("How Many Nurses are Available for Advice?", 1, 10, step=1, value=3)
+            nurses_treat = st.slider("How Many Nurses are Available for Treatment?", 1, 10, step=1, value=4)
+
+        else:
+            nurses = st.slider("How Many Rooms/Nurses Are Available?", 1, 10, step=1, value=7)
+
+        with st.expander("Previous Parameters"):
+
+            st.markdown("If you like, you can edit these parameters too!")
+
+            seed = st.number_input("Set a random number for the computer to start from",
+                            1, 10000000,
+                            step=1, value=42)
+            
+            n_reps = st.slider("How many times should the simulation run?",
+                            1, 30,
+                            step=1, value=10)
+            
+            run_time_days = st.slider("How many days should we run the simulation for each time?",
+                                    1, 60,
+                                    step=1, value=15)
+
+        
+            mean_arrivals_per_day = st.slider("How many patients should arrive per day on average?",
+                                            10, 500,
+                                            step=5, value=300)    
+
+            consult_time = st.slider("How long (in minutes) does a consultation take on average?",
+                                        5, 120, step=5, value=50)
+
+            consult_time_sd = st.slider("How much (in minutes) does the time for a consultation usually vary by?",
+                                        5, 30, step=5, value=10)
+
+        with col2:
+            # st.markdown("Placeholder")
+            # args = Scenario(
+            #             random_number_set=seed,
+            #             n_cubicles_1=nurses,
+            #             override_arrival_lambda=True,
+            #             manual_arrival_lambda=60/(mean_arrivals_per_day/24),
+            #             model="simplest",
+            #             trauma_treat_mean=consult_time,
+            #             trauma_treat_var=consult_time_sd
+            #             )
+
+            # n_reps = 8
+
+            # # A user must press a streamlit button to run the model
+            button_run_pressed = st.button("Run simulation")
+            
+            
+            # if button_run_pressed:
+
+            #     # add a spinner and then display success box
+            #     with st.spinner('Simulating the minor injuries unit...'):
+            #         # run multiple replications of experment
+            #         detailed_outputs = multiple_replications(
+            #             args,
+            #             n_reps=n_reps,
+            #             rc_period=30*60*24,
+            #             return_detailed_logs=True
+            #         )
