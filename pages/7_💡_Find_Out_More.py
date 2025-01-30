35834efb626f010f27a82cfb6a143802273a368a
Sammi Rosser <samanthajaderosser@gmail.com>
2025-01-30 12:23:30 +0000

    Update links page

--- pages/7_💡_Find_Out_More.py
+++ pages/7_💡_Find_Out_More.py
@@ -28,16 +28,16 @@ with open("style.css") as css:
 ## We add in a title for our web app's page
 st.title("Discrete Event Simulation Playground")
 
-st.subheader("Simulation Modelling in the HSMA")
+st.subheader("Simulation Modelling in the HSMA Programme")
 
 gc.collect()
 
 st.markdown(
     """
-    Joining the HSMA programme will give you access to
-    - three sessions on discrete event simulation
+    Tthe HSMA programme contains
+    - three sessions on discrete event simulation (the technique used in this web app)
     - a session on system dynamics modelling (for large, system-scale problems)
-    - sessions on cellular automata and agent-based simulation (for modelling interactions and motivations of individuals that lead to high-level patterns, e.g. to look at the spread of disease)
+    - sessions on agent-based simulation (for modelling interactions and motivations of individuals that lead to high-level patterns, e.g. to look at the spread of disease)
     - sessions on creating web-based interfaces (like this one!) for allowing users to interact with your models
     """
 )
@@ -47,31 +47,56 @@ st.subheader("Where Can I Find Out More?")
 
 st.markdown(
     """
-    If you just can't wait until the next round of HSMA, you can access the previous videos on discrete event simulation below.
+    If you just can't wait until the next round of HSMA, you can access the previous content on discrete event simulation below.
 
-    However - if you apply to HSMA 6, you will get the benefit of support from the HSMA team as well as a peer support group.
+    However - if you apply to HSMA, you will get the benefit of support from the HSMA team as well as a peer support group.
 
-    We're always revising our content, so HSMA 6 will be bigger and better than ever before!
+    Take a look at our full DES module page on our website [here](https://hsma.co.uk/hsma_content/modules/current_module_details/2_des.html).
     """
 )
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.subheader("Video 1")
-    st.video("https://www.youtube.com/watch?v=IlUPJcowBrA")
+    st.subheader("An Introduction to DES Concepts")
+    st.video("https://www.youtube.com/watch?v=TxdvJQxuSx4")
 
 with col2:
-    st.subheader("Video 2")
-    st.video("https://www.youtube.com/watch?v=F3av_4iJMzA")
+    st.subheader("Starting to code DES models with Simpy")
+    st.video("https://www.youtube.com/watch?v=nHgyJ6i0TGs")
 
+with col3:
+    st.subheader("Coding More Complex Videos in Simpy")
+    st.video("https://www.youtube.com/watch?v=FBVBKtVkayA")
+
+
+st.header("The DES Book")
+
+image_col, text_col = st.columns([0.4, 0.6])
+
+with image_col:
+    st.image("https://raw.githubusercontent.com/hsma-programme/Teaching_DES_Concepts_Streamlit/refs/heads/main/resources/des_cover_image.jpeg")
+
+with text_col:
+    st.markdown("""
+    There's so much to teach you about DES that we just couldn't fit it all into our sessions - so we wrote a book!
+
+    The DES book is a free eBook that takes you from your first models through to much more complex modelling concepts than we cover in the taught sessions.
+
+    We'd love to hear from you if you find the book useful, or if there are additional concepts you would like it to cover.
+    """)
+
+st.header("HSMA DES Projects")
 
 st.markdown(
     """
     Several HSMA projects have made use of discrete event simulation to motivate incredible changes in health systems.
+
+    You can view some of them on our [projects page](https://hsma.co.uk/previous_projects/projects_by_methods.html#discrete-event-simulation)
     """
 )
 
+st.header("Building Web Apps Like This One")
 
 st.markdown(
     """
@@ -79,7 +104,9 @@ st.markdown(
     Finally, this whole exercise website has been written in Streamlit - another topic we cover on the course!
     Streamlit allows you to create highly customisable websites that allow you to share results with users and give them the freedom to interact with powerful Python code without needing Python on their own computers.
 
-    Two brand new sessions on Streamlit are being created for HSMA 6 - so apply now if you want to find out more!
+    You can view our streamlit training [here](https://hsma.co.uk/hsma_content/modules/current_module_details/7_git_and_web_development.html)
+
+    Session 7B gets you started on writing Streamlit apps, and 7C helps you to build and deploy a discrete event simulation front-end just like this one!
     """
 )
 
